dabedf5c4419c4af1ee6b4412e7967d7b92fd071
teo3300 <matteo.rogora@live.it>
2021-05-30 01:15:04 +0200

    never ending suffer

--- header.h
+++ header.h
@@ -10,6 +10,7 @@
 #define print
 
 #define ind(y, x) (y*nodes_cnt+x)                                 // usato per trattare vettore dei vertici come una matrice
+#define adr(matrix, y, x)   ( (matrix) + (y) * nodes_cnt + (x))
 
 /********** header.h **********/
 
--- openPart.c
+++ openPart.c
@@ -1,21 +1,24 @@
-uint nodes_cnt;                                                     // perdoname madre por mi variable non locale (da usare in ind(y,x))
+uint nodes_cnt;                                                         // perdoname madre por mi variable non locale (da usare in ind(y,x))
+
+void parse();
 
 int main(){
     uint topK,
          cnt = 0;
-    char command_buffer[COMMAND_BUFF_LEN];                          // temporaneo tampone per distinguere i comandi
+    char command_buffer[COMMAND_BUFF_LEN];                              // temporaneo tampone per distinguere i comandi
     scanf("%u %u\n",&nodes_cnt, &topK);
     //PRINT(nodes_cnt);
     //PRINT(topK);
 
-    uint graph_matrix[nodes_cnt*nodes_cnt];                         // salva la matrice di incidenza come un VLA
+    uint graph_matrix[nodes_cnt*nodes_cnt];                             // salva la matrice di incidenza come un VLA
 
     while(scanf("%s\n", command_buffer)>0){
         if((*command_buffer) == 'A'){
             cnt++;
             for(int i=0; i<nodes_cnt; i++){
                 for(int j=0; j<nodes_cnt; j++){
-                    scanf("%u,", &graph_matrix[ind(i,j)]);           // TODO: possibile operare su matrice incompleta?
+                    parse(adr(graph_matrix,i,j));
+                    //parse(&graph_matrix[ind(i,j)]);                     // TODO: si può migliorare ancora?
                     //printf("%5u ", graph_matrix[ind(i,j)]);
                 }
             //printf("\n");
@@ -26,6 +29,15 @@ int main(){
         }
     }
     return 0;
+}
+
+void parse(uint* ret){                                                  // scanf merda
+    *ret=0;
+    int c = getchar();
+    while(c != '\n' && c != ','){
+        *ret = (*ret)*10 + c -'0';
+        c = getchar();
+    }
 }
 
 /********** OpenPart.c **********/
