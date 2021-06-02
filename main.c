0d0313a82a9ba1045b42368d58626d7672859936
teo3300 <matteo.rogora@live.it>
2021-06-02 19:59:17 +0200

    costruita pila per classifica e notato un errorone

--- main.c
+++ main.c
@@ -16,10 +16,11 @@
 #define SMALLER(a,b)    (heap[(a)].dist < heap[(b)].dist)                           // per tenere separato il criterio di ordinamento
 #define SWAP(a,b)       {Nodo C = heap[(a)]; heap[(a)] = heap[(b)]; heap[(b)] = C;}
 
+//  Utilizza la stessa definizione sia per i dati che per i metadati
 typedef union{
     struct{
-        uint labl;
         uint dist;
+        uint labl;
     };
     struct{
         uint length;
@@ -28,12 +29,24 @@ typedef union{
 }Nodo;
 typedef Nodo* Heap;
 
+typedef union NodoLista{
+    struct{
+        uint peso;
+        union NodoLista* next;
+    };
+    struct{
+        uint length;
+        union NodoLista* first;
+    };
+}nodoLista;
+typedef nodoLista* Pila;
+
 uint**  allocaMatrice(uint dim);
 uint**  liberaMatrice(uint** matrice, uint dim);
 void    riempiMatrice(uint** matrice, uint dim);
 void    stampaMatrice(uint** matrice, uint dim);
 uint    calcolaMinSpanTree(uint** matrice, Heap heap, uint dim);
-Heap    allocaMinHeap(uint dim);
+Heap    allocaHeap(uint dim);
 Heap    liberaMinHeap(Heap heap);
 void    costruisciMinHeap(Heap heap);
 void    minHeapify(Heap heap, uint n);
@@ -44,41 +57,67 @@ void    inserisciHeap(Heap heap, uint labl, uint dist);
 void    stampaMinHeap(Heap heap);
 void    stampaResiduo(Heap heap);
 uint    DijkstraQueue(uint** matrice, Heap heap, uint dim);
-
-int dump;
+uint    DijkstraQuad(uint** matrice, Heap heap, uint dim);
+Pila    creaPila();
+void    pushPila(Pila pila, uint val);
+uint    popPila(Pila pila);
+uint    dimensionePila(Pila);
 
 int main(){
-    uint    indice = 0;
+    uint    indice = 0, heap_top = 0;
+    uint    peso_grafo;
     uint    numero_nodi;
-    uint    lunghezza_classifica;
+    uint    K;
     char    comando[LUNGHEZZA_MAX_COMANDI];
     uint**  matrice_adiacenza;
     Heap    heap_supporto;
+    Heap    heap_classifica;
+    Pila    top_pila;
     //*leggere i valori di dimensione dei grafi e della lunghezza della classifica
     //*leggere comando
     //*se "AggiungiGrafo":
     //*     riempi la matrice di adiacenza coi valori inseriti
-    //      calcola l'albero dei cammini minimi
-    //      aggiorna la classifica
+    //*     calcola l'albero dei cammini minimi
+    //      se #grafi < dimensione top:
+    //          push grafo
+    //      se #grafi > dimensione top:
+    //          copia pila su max heap
+    //          ordina heap
+    //          se peso nuovo grafo < peso max:
+    //              rimuovi max, e aggiungi nuovo (con rispettive max heapyfy e bubble up)
     //*se "TopK":
-    //      stampa la classifica
+    //      se #grafi < dimensione top:
+    //          stampa lista
+    //      se #grafi > dimensione top
+    //          stampa heap
     //*se sconosciuto o finiti gli input esci altrimenti leggi nuovo comando
 
-    if(scanf("%u %u\n", &numero_nodi, &lunghezza_classifica) == EOF) return 1; 
+    if(scanf("%u %u\n", &numero_nodi, &K) == EOF) return 1; 
     //printf("Nodi: %u, Classifica: %u\n", numero_nodi, lunghezza_classifica);
     matrice_adiacenza = allocaMatrice(numero_nodi);
-    heap_supporto     = allocaMinHeap(numero_nodi);                                 // heap non deve contenere il nodo 0
-
+    heap_supporto     = allocaHeap(numero_nodi-1);                                    // heap non deve contenere il nodo 0
+    top_pila          = creaPila();
 
     while(scanf("%s\n", comando) != EOF){
         if(! strncmp(comando, AggiungiGrafo, LUNGHEZZA_MAX_COMANDI)){
             riempiMatrice(matrice_adiacenza, numero_nodi);                          // stampaMatrice(matrice_adiacenza, numero_nodi);
-            printf("%2u %u\n", indice, calcolaMinSpanTree(matrice_adiacenza, heap_supporto, numero_nodi));
-            //calcolaMinSpanTree(matrice_adiacenza, heap_supporto, numero_nodi);
+            //printf("%4u %u\n", indice, calcolaMinSpanTree(matrice_adiacenza, heap_supporto, numero_nodi));
+            peso_grafo = calcolaMinSpanTree(matrice_adiacenza, heap_supporto, numero_nodi);
+            if(indice < K){
+                pushPila(top_pila, peso_grafo);
+            }else if(indice == K){
+                heap_classifica = allocaHeap(K);
+                heap_top = 1;
+            }
             indice++;
         }else if(! strncmp(comando, TopK, LUNGHEZZA_MAX_COMANDI)){
-
+            while(dimensionePila(top_pila)){
+                printf("Pop da pila (%u): %u\n", --indice, popPila(top_pila));
+            }
         }else return 1;                                                             // comando inatteso
+    }
+    if(heap_top){
+        free(heap_classifica);
     }
     heap_supporto     = liberaMinHeap(heap_supporto);
     matrice_adiacenza = liberaMatrice(matrice_adiacenza, numero_nodi);
@@ -106,7 +145,7 @@ uint** liberaMatrice(uint** matrice, uint dim){
 void riempiMatrice(uint** matrice, uint dim){
     for(int i=0; i<dim; i++){
         for(int j=0; j<dim; j++){
-            if(scanf("%10u,", &matrice[i][j]));
+            if(scanf("%u,", &matrice[i][j]));
             if(!matrice[i][j]) matrice[i][j] = INFINITO;
         }
     }
@@ -133,12 +172,13 @@ uint calcolaMinSpanTree(uint** matrice, Heap heap, uint dim){
     }
     costruisciMinHeap(heap);
     return DijkstraQueue(matrice, heap, dim);
+    //return DijkstraQuad(matrice, heap, dim);
 }
 
 uint DijkstraQueue(uint** matrice, Heap heap, uint dim){
     uint ret = 0;
     Nodo tmp;
-    while (cancellaMin(heap, &tmp) && tmp.size < INFINITO){
+    while (cancellaMin(heap, &tmp) && tmp.dist < INFINITO){
         for(uint i=1; i<=HEAP.size; i++){
             uint step = matrice[tmp.labl][heap[i].labl];
             if(step < INFINITO && tmp.dist + step < heap[i].dist){
@@ -155,11 +195,45 @@ uint DijkstraQueue(uint** matrice, Heap heap, uint dim){
     return ret;
 }
 
+uint DijkstraQuad(uint** matrice, Heap heap, uint dim){
+    uint ret = 0;
+    uint distTmp = 0;
+    uint min = 1;
+    uint mindist = INFINITO;
+    while(1){
+        uint checkAllZeroOrInfinite = 0;
+        for(uint i=1; i<dim && !checkAllZeroOrInfinite; i++){
+            if(heap[i].dist != 0 && heap[i].dist != INFINITO){
+                checkAllZeroOrInfinite = 1;
+            }
+        }
+        if(!checkAllZeroOrInfinite) return ret;
+        for(uint i=1; i<dim; i++){
+            if(heap[i].dist != 0 && heap[i].dist < mindist){        // ignorare nodi rimossi
+                min = i;                                            // aggiorna max
+                mindist = heap[min].dist;                           // e il suo valore
+            }
+        }
+        ret += heap[min].dist;                                       // salva cammino minimo
+        distTmp = heap[min].dist;                                    // salva dist per calcolo
+        heap[min].dist = 0;                                          // rimuovi nodo
+        mindist = INFINITO;                                          // riprepara per prossimo ciclo
+        for(uint i=1; i<dim; i++){
+            if(i != min && heap[i].dist != 0){                       // ignora autoanelli e nodi rimossi
+                uint step = matrice[min][i];
+                if(distTmp + step < heap[i].dist && step < INFINITO){
+                    heap[i].dist = distTmp + step;
+                }
+            }
+        }
+    }
+}
+
 /********************* heap *****************************/
 
-Heap allocaMinHeap(uint dim){
-    Heap tmp = (Heap) malloc((dim)*sizeof(Nodo));  // rende lo heap 1-based e utilizza heap[0] per salvare lunghezza e dimensione
-    tmp[0].length = dim-1;
+Heap allocaHeap(uint dim){
+    Heap tmp = (Heap) malloc((dim+1)*sizeof(Nodo));  // rende lo heap 1-based e utilizza heap[0] per salvare lunghezza e dimensione
+    tmp[0].length = dim;
     return tmp;
 }
 
@@ -192,7 +266,6 @@ void minHeapify(Heap heap, uint n){
 
 void controllaMin(Heap heap, Nodo* ret){
     *ret = heap[1];
-    //*ret = (Nodo){heap[1].labl, heap[1].dist};
 }
 
 uint cancellaMin(Heap heap, Nodo* ret){
@@ -230,4 +303,32 @@ void stampaResiduo(Heap heap){
     for(int i=HEAP.size+1; i<=HEAP.length; i++){
         printf("(%3u,%10u)\n", heap[i].labl, heap[i].dist);
     }
+}
+
+Pila creaPila(){
+    Pila pila = (Pila) malloc(sizeof(nodoLista));
+    pila->length = 0;
+    pila->next = NULL;
+    return pila;
+}
+
+void pushPila(Pila pila, uint valore){
+    nodoLista* nodo = (nodoLista*) malloc(sizeof(nodoLista));
+    nodo->peso = valore;
+    nodo->next = pila->first;
+    pila->first = nodo;
+    pila->length++;
+}
+
+uint popPila(Pila pila){
+    uint ret = pila->first->peso;
+    nodoLista* ToS = pila->first->next;
+    free(pila->first);
+    pila->first = ToS;
+    pila->length--;
+    return ret;
+}
+
+uint dimensionePila(Pila pila){
+    return pila->length;
 }
